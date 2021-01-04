b6e2828509c19288d92406c1f64397fa27092a6c
myuon <ioi.joi.koi.loi@gmail.com>
2021-01-04 14:02:54 +0900

    agc024c

---
+++ agc024/src/bin/c.rs
@@ -0,0 +1,80 @@
+#[macro_export]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+#[macro_export]
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+#[macro_export]
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn solve(an: Vec<i64>) -> i64 {
+    let mut result = 0;
+    let mut prev = -1;
+    for a in an {
+        if a > prev + 1 {
+            return -1;
+        }
+        if a <= prev {
+            result += a;
+        }
+        if a == prev + 1 && a > 0 {
+            result += 1;
+        }
+
+        prev = a;
+    }
+
+    result
+}
+
+fn main() {
+    input! {
+        n: usize,
+        an: [i64;n],
+    }
+
+    println!("{}", solve(an));
+}
