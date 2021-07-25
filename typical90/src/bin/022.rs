1a880ae01cb3afafa7b43f8924649224c6a8c3f5
myuon <ioi.joi.koi.loi@gmail.com>
2021-07-26 00:21:52 +0900

    typical90-022

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,3 +1,18 @@
+use algo::*;
+use num::integer::gcd;
+
+fn solve(a: i64, b: i64, c: i64) -> i64 {
+    let l = gcd(gcd(a, b), c);
+
+    (a / l - 1) + (b / l - 1) + (c / l - 1)
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+    }
+
+    println!("{}", solve(a, b, c));
 }
