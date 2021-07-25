c2b0747ce4df5714923e3907fab8bec4916afaef
myuon <ioi.joi.koi.loi@gmail.com>
2021-07-26 00:27:21 +0900

    typical90-027

--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -1,3 +1,32 @@
+use std::collections::HashSet;
+
+use algo::*;
+
+fn solve(n: usize, sn: Vec<String>) -> Vec<usize> {
+    let mut cache = HashSet::new();
+    let mut result = Vec::new();
+
+    for i in 0..n {
+        let si = &sn[i];
+        if cache.contains(si) {
+            continue;
+        }
+
+        result.push(i);
+        cache.insert(si.clone());
+    }
+
+    result
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        sn: [String; n],
+    }
+
+    let result = solve(n, sn);
+    for i in result {
+        println!("{}", i + 1);
+    }
 }
