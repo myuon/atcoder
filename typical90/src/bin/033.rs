fbd1731c4bc42ac797ee20815fd8d55f8474c7ca
myuon <ioi.joi.koi.loi@gmail.com>
2021-07-28 03:14:47 +0900

    typical90-033

--- typical90/src/bin/033.rs
+++ typical90/src/bin/033.rs
@@ -1,3 +1,18 @@
+use algo::*;
+
+fn solve(h: i32, w: i32) -> i32 {
+    if h == 1 || w == 1 {
+        h.max(w)
+    } else {
+        ((h + 1) / 2) * ((w + 1) / 2)
+    }
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: i32,
+        w: i32
+    }
+
+    println!("{}", solve(h, w));
 }
