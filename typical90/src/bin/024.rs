afffc1a727846dd607d91886126e07b55ef64324
myuon <ioi.joi.koi.loi@gmail.com>
2021-07-28 03:01:35 +0900

    typical90-024

--- typical90/src/bin/024.rs
+++ typical90/src/bin/024.rs
@@ -1,3 +1,21 @@
+use algo::*;
+
+fn solve(n: usize, k: usize, an: Vec<i32>, bn: Vec<i32>) -> bool {
+    let mut diff = 0;
+    for i in 0..n {
+        diff += (an[i] - bn[i]).abs();
+    }
+
+    diff <= k as i32 && (k as i32 - diff).abs() % 2 == 0
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        an: [i32; n],
+        bn: [i32; n],
+    }
+
+    println!("{}", if solve(n, k, an, bn) { "Yes" } else { "No" });
 }
