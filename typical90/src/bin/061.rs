7eb23e9423d1dc6d1f8ac9a608d9e190d2028601
myuon <ioi.joi.koi.loi@gmail.com>
2021-07-31 18:49:36 +0900

    typical90-061

--- typical90/src/bin/061.rs
+++ typical90/src/bin/061.rs
@@ -1,3 +1,31 @@
+use std::collections::VecDeque;
+
+use algo::*;
+
+fn solve(q: usize, txn: Vec<(i32, i64)>) -> Vec<i64> {
+    let mut acc = Vec::new();
+    let mut deck = VecDeque::new();
+
+    for (t, x) in txn {
+        if t == 1 {
+            deck.push_front(x);
+        } else if t == 2 {
+            deck.push_back(x);
+        } else if t == 3 {
+            acc.push(deck[x as usize - 1]);
+        }
+    }
+
+    acc
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        q: usize,
+        txn: [(i32, i64); q]
+    }
+
+    for v in solve(q, txn) {
+        println!("{}", v);
+    }
 }
