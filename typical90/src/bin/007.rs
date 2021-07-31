7bc152dd492d147b5cf28c7ec5069c42ef6531e0
myuon <ioi.joi.koi.loi@gmail.com>
2021-07-31 21:43:03 +0900

    typical90-007

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -1,3 +1,33 @@
+use algo::*;
+
+fn solve(n: usize, an: Vec<i64>, q: usize, bn: Vec<i64>) -> Vec<i64> {
+    let mut result = vec![];
+    let mut cs = an;
+    cs.sort();
+
+    for b in bn {
+        let i = cs.binary_search(&b).unwrap_or_else(|t| t);
+        result.push(if i == 0 {
+            cs[i] - b
+        } else if i == n {
+            b - cs[i - 1]
+        } else {
+            (cs[i] - b).min(b - cs[i - 1])
+        });
+    }
+
+    result
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        an: [i64; n],
+        q: usize,
+        bn: [i64; q],
+    }
+
+    for b in solve(n, an, q, bn) {
+        println!("{}", b);
+    }
 }
