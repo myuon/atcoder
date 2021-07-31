bf662bbca7d54829198f6b335ccbfd005fa063ca
myuon <ioi.joi.koi.loi@gmail.com>
2021-07-31 18:43:51 +0900

    typical90-055

--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -1,3 +1,33 @@
+use algo::*;
+
+fn solve(n: usize, p: i64, q: i64, an: Vec<i64>) -> i64 {
+    let mut c = 0;
+
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                for l in k + 1..n {
+                    for m in l + 1..n {
+                        if (((((((an[i] * an[j]) % p) * an[k]) % p) * an[l]) % p) * an[m]) % p == q
+                        {
+                            c += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    c
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        p: i64,
+        q: i64,
+        an: [i64; n],
+    }
+
+    println!("{}", solve(n, p, q, an));
 }
