27eae460bde66f806b52c10a85f367c268ff9f79
myuon <ioi.joi.koi.loi@gmail.com>
2021-07-31 20:32:25 +0900

    typical90-002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -1,3 +1,37 @@
+use algo::*;
+
+fn dfs(n: usize, score: usize, prefix: String, result: &mut Vec<String>) {
+    if n == prefix.len() {
+        result.push(prefix);
+        return;
+    }
+
+    if score < n - prefix.len() {
+        dfs(n, score + 1, prefix.clone() + "(", result);
+    }
+    if score > 0 {
+        dfs(n, score - 1, prefix + ")", result);
+    }
+}
+
+fn solve(n: usize) -> Vec<String> {
+    let mut result = vec![];
+
+    if n % 2 == 1 {
+        return vec![];
+    }
+
+    dfs(n, 0, String::new(), &mut result);
+
+    result
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    }
+
+    for i in solve(n) {
+        println!("{}", i);
+    }
 }
