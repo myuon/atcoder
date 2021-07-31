363eb85bcb698c9b59947671c36adf8d40dbd50f
myuon <ioi.joi.koi.loi@gmail.com>
2021-07-31 20:15:06 +0900

    typical90-078

--- typical90/src/bin/078.rs
+++ typical90/src/bin/078.rs
@@ -1,3 +1,28 @@
+use algo::*;
+
+fn solve(n: usize, m: usize, abs: Vec<(i32, i32)>) -> i32 {
+    let mut graph = vec![vec![]; n];
+
+    for (a, b) in abs {
+        graph[a.max(b) as usize - 1].push(a.min(b) - 1);
+    }
+
+    let mut c = 0;
+    for i in 0..n {
+        if graph[i].len() == 1 {
+            c += 1;
+        }
+    }
+
+    c
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        abs: [(i32, i32); m]
+    }
+
+    println!("{}", solve(n, m, abs));
 }
