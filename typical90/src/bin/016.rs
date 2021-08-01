c413f62a57f764957c7def97ac3d7e165e78a5ee
myuon <ioi.joi.koi.loi@gmail.com>
2021-08-01 16:20:37 +0900

    typical90-016

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -1,3 +1,30 @@
+use proconio::*;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: i64,
+        a: i64,
+        b: i64,
+        c: i64,
+    }
+
+    let max_sum = 10000;
+    let mut m = 1_000_000;
+    for x in 0..max_sum {
+        if a * x > n {
+            continue;
+        }
+
+        for y in 0..(max_sum - x) {
+            if a * x + b * y > n {
+                continue;
+            }
+
+            if (n - (a * x + b * y)) % c == 0 {
+                m = m.min(x + y + (n - (a * x + b * y)) / c);
+            }
+        }
+    }
+
+    println!("{}", m);
 }
