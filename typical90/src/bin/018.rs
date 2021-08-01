bd19a315e83c3e6e45f60c081108b5cd6ad2d590
myuon <ioi.joi.koi.loi@gmail.com>
2021-08-01 16:41:12 +0900

    typical90-018

--- typical90/src/bin/018.rs
+++ typical90/src/bin/018.rs
@@ -1,3 +1,21 @@
+use std::f64::consts::PI;
+
+use proconio::*;
+
 fn main() {
-    unimplemented!();
+    input! {
+        t: i64,
+        l: i64,
+        x: i64,
+        y: i64,
+        q: usize,
+        es: [i64; q]
+    }
+
+    for e in es {
+        let theta = 2.0 * PI * (e as f64) / (t as f64);
+        let u = (l as f64 / 2.0) * (1.0 - theta.cos());
+        let v = (x.pow(2) as f64 + (y as f64 - (l as f64 / 2.0) * (-theta.sin())).powi(2)).sqrt();
+        println!("{}", u.atan2(v).to_degrees());
+    }
 }
