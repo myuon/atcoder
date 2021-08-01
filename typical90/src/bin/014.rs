ee4050618599b19dd39c0cda3bf3f212db02ffaf
myuon <ioi.joi.koi.loi@gmail.com>
2021-08-01 15:59:53 +0900

    typical90-014

--- typical90/src/bin/014.rs
+++ typical90/src/bin/014.rs
@@ -1,3 +1,19 @@
+use proconio::*;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut an: [i64; n],
+        mut bn: [i64; n],
+    }
+
+    an.sort();
+    bn.sort();
+
+    let mut result = 0;
+    for i in 0..n {
+        result += (an[i] - bn[i]).abs();
+    }
+
+    println!("{}", result);
 }
