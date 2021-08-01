58f3cbb385828a12155a2b6d85fe5fb39c3c66c3
myuon <ioi.joi.koi.loi@gmail.com>
2021-08-01 17:32:32 +0900

    typical90-038

--- typical90/src/bin/038.rs
+++ typical90/src/bin/038.rs
@@ -1,3 +1,19 @@
+use num::Integer;
+use proconio::*;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: i64,
+        b: i64,
+    }
+
+    let large: i64 = 1000000000000000000;
+    let gcd = a.gcd(&b);
+    let lcm = (a / gcd) * b;
+
+    if a / gcd <= large / b {
+        println!("{}", lcm);
+    } else {
+        println!("Large");
+    }
 }
