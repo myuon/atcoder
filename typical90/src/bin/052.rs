a5d6b52eca8a92babd73f08be6bd2a7605dc1e61
myuon <ioi.joi.koi.loi@gmail.com>
2022-01-09 01:57:53 +0900

    typical90-052

--- typical90/src/bin/052.rs
+++ typical90/src/bin/052.rs
@@ -1,3 +1,24 @@
+use algo::*;
+
+fn solve(n: usize, aij: Vec<Vec<usize>>) -> i64 {
+    let mut result = 1;
+    for i in aij {
+        let mut s = 0;
+        for j in i {
+            s += j;
+        }
+
+        result = (result * s as i64) % (1000000000 + 7);
+    }
+
+    result
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        aij: [[usize; 6]; n],
+    }
+
+    println!("{}", solve(n, aij));
 }
