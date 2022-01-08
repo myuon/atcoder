ef01021b660ca8e3314bfb7414796958165ee959
myuon <ioi.joi.koi.loi@gmail.com>
2022-01-09 01:15:27 +0900

    typical90-048

--- typical90/src/bin/048.rs
+++ typical90/src/bin/048.rs
@@ -1,3 +1,31 @@
+use std::collections::BinaryHeap;
+
+use algo::*;
+
+fn solve(n: usize, k: usize, abn: Vec<(usize, usize)>) -> i64 {
+    let mut result = 0;
+    let mut scores = BinaryHeap::new();
+    for (i, (a, b)) in abn.iter().enumerate().collect::<Vec<_>>() {
+        scores.push((*b, i, 1));
+    }
+
+    for _ in 0..k {
+        let (p, i, k) = scores.pop().unwrap();
+        if k == 1 {
+            scores.push((abn[i].0 - abn[i].1, i, 2));
+        }
+        result += p as i64;
+    }
+
+    result
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        abn: [(usize, usize); n],
+    }
+
+    println!("{}", solve(n, k, abn));
 }
