1ae8216f6fd1095fc208a1210fb25eee0a9ab0f6
myuon <ioi.joi.koi.loi@gmail.com>
2022-01-09 01:47:37 +0900

    typical90-052

--- typical90/src/bin/050.rs
+++ typical90/src/bin/050.rs
@@ -1,3 +1,22 @@
+use algo::*;
+
+fn solve(n: usize, l: usize) -> i64 {
+    let mut dp = vec![0; n + 1];
+
+    dp[0] = 1;
+    for i in 1..=n {
+        dp[i] = (if i >= 1 { dp[i - 1] } else { 0 } + if i >= l { dp[i - l] } else { 0 })
+            % (1000000000 + 7);
+    }
+
+    dp[n]
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        l: usize
+    }
+
+    println!("{}", solve(n, l));
 }
